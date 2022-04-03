1987b660035f22f580acd76da21b5606a359d68d
Dong-Geon Kim <dgkim0813@gmail.com>
2022-04-04 00:26:25 +0900

    Add files
    license, notice, edited train

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
+# Modified by Dong-Geon Kim
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
 from functools import partial
 
 import tensorflow as tf
@@ -37,7 +43,8 @@ When training your own target detection model, you must pay attention to the fol
 gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
-    
+
+
 if __name__ == "__main__":
     #------------------------------------------------------#
     #   Whether to use eager mode training
